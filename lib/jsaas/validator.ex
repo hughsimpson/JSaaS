a2b5c9d819e4b9a530955b3fceb56a724d8d0b2c
Hugh Simpson <hugh.simpson@babylonhealth.com>
2017-06-21 00:07:30 +0100

    more tests

--- lib/jsaas/validator.ex
+++ lib/jsaas/validator.ex
@@ -1,4 +1,21 @@
 defmodule JSaaS.Validator do
+  @doc """
+      iex> JSaaS.Validator.is_not_uri?("http://foo.bar")
+      false
+
+      iex> JSaaS.Validator.is_not_uri?("www.example.com")
+      true
+
+      iex> JSaaS.Validator.is_not_uri?("another.hmm://x.y")
+      true
+
+      iex> JSaaS.Validator.is_not_uri?("git://lol.ok")
+      false
+
+      iex> JSaaS.Validator.is_not_uri?("https://min.to/right?q=param")
+      false
+  """
+  @spec is_not_uri?(String.t) :: boolean
   def is_not_uri?(text) do
     uri = URI.parse(text)
     uri.scheme == nil || false
